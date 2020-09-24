f88e3f050ec793b70552d6caded727a8d820f55d
eugenl <eugenelukelee@gmail.com>
2020-09-24 14:53:11 -0700

    links

--- src/components/pages/GuidesItem.js
+++ src/components/pages/GuidesItem.js
@@ -8,9 +8,9 @@ class GuidesItem extends Component {
             <div className="row align-items-start text-center">
             {this.props.guides.map((guide) => (
                 <div className="col-sm-4 mb-4" key={ guide.secondText }>
-                    <a href={ guide.articleLink } target="_blank" rel="noopener noreferrer"><img src={ process.env.PUBLIC_URL + guide.imgLink } style={{ height:"352px", width:"100%" }} alt={ guide.firstText }></img></a>
+                    <a href={ guide.articleLink }><img src={ process.env.PUBLIC_URL + guide.imgLink } style={{ height:"352px", width:"100%" }} alt={ guide.firstText }></img></a>
                     <span className="first-text">{ guide.firstText }</span>
-                    <span className="second-text"><a href={ guide.articleLink } target="_blank" rel="noopener noreferrer">{ guide.secondText }</a></span>
+                    <span className="second-text"><a href={ guide.articleLink }>{ guide.secondText }</a></span>
                     <span>
                         { guide.spanBody }
                     </span>
--- src/components/pages/TravelsItem.js
+++ src/components/pages/TravelsItem.js
@@ -6,8 +6,8 @@ class TravelsItem extends Component {
         return this.props.travels.map((travel) => (
             travel.continent === continent ?
             <div className="col-sm-3 mb-4" key={ travel.city }>
-                <a href={ travel.link } target="_blank" rel="noopener noreferrer"><img src={ process.env.PUBLIC_URL + travel.image } style={{ height:"240px", width:"100%" }} alt={ travel.city }></img></a>
-                <span id="cities"><a href={ travel.link } target="_blank" rel="noopener noreferrer">{ travel.city }</a></span>
+                <a href={ travel.link }><img src={ process.env.PUBLIC_URL + travel.image } style={{ height:"240px", width:"100%" }} alt={ travel.city }></img></a>
+                <span id="cities"><a href={ travel.link }>{ travel.city }</a></span>
                 <span id="countries">{ travel.country }</span>
             </div>
             : ''
