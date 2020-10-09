d92fa261a2f2d806556bb1696bdb386dee19bf6f
eugenl <eugenelukelee@gmail.com>
2020-10-08 21:13:17 -0700

    mailchimp

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Mailchimp from 'react-mailchimp-form';
 
 
 class Services extends Component {
@@ -38,40 +39,36 @@ class Services extends Component {
         </div>
 
         <div id="form-wrapper">
-
-          <div id="mc_embed_signup">
-
-            <form action="https://neverendingcycle.us17.list-manage.com/subscribe/post?u=ba2ce2d903727946d9203b1af&amp;id=efae2ade1d" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-              <div id="mc_embed_signup_scroll">
-                <h2>Subscribe</h2>
-                <div className="indicates-required"><span class="asterisk">*</span> indicates required</div>
-
-                <div className="mc-field-group">
-                  <label for="mce-EMAIL">Email Address <span class="asterisk">*</span></label>
-                  <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL"/>
-                </div>
-
-                <div className="mc-field-group">
-                  <label for="mce-FNAME">First Name </label>
-                  <input type="text" value="" name="FNAME" class="" id="mce-FNAME"/>
-                </div>
-
-                <div className="mc-field-group">
-                  <label for="mce-LNAME">Last Name </label>
-                  <input type="text" value="" name="LNAME" class="" id="mce-LNAME"/>
-                </div>
-
-                <div id="mce-responses" class="clear">
-                  <div className="response" id="mce-error-response" style={{ display:"none" }}></div>
-                  <div className="response" id="mce-success-response" style={{ display:"none" }}></div>
-                </div>    
-                <div style= {{ position: "absolute", left: "-5000px" }}  aria-hidden="true"><input type="text" name="b_ba2ce2d903727946d9203b1af_efae2ade1d" tabindex="-1" value=""/></div>
-                <div className="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"/></div>
-              </div>
-            </form>
-
-          </div>
-
+          <Mailchimp
+            action='https://neverendingcycle.us17.list-manage.com/subscribe/post?u=ba2ce2d903727946d9203b1af&amp;id=efae2ade1d' 
+            
+            //Adding multiple fields:
+            fields={[
+              {
+                name: 'EMAIL',
+                placeholder: 'Email',
+                type: 'email',
+                required: true
+              },
+              {
+                name: 'FNAME',
+                placeholder: 'name',
+                type: 'text',
+                required: true
+              }
+            ]}
+            // Change predetermined language
+            messages = {
+              {
+                sending: "Sending...",
+                success: "Thank you for subscribing!",
+                error: "An unexpected internal error has occurred.",
+                empty: "You must provide an e-mail.",
+                duplicate: "Too many subscribe attempts for this email address",
+                button: "Subscribe!"
+              }
+            }
+          />
         </div>
 
      </div>
